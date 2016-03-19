aa3d16dd5cb8516b8a08b274a25bd8b1fb662a7a
Connor <connorlurring@gmail.com>
2016-03-19 20:18:52 +0000

    Added todos

--- routes/api.js
+++ routes/api.js
@@ -5,20 +5,20 @@ router.get('/:devicekey/temp/:type', function(req, res, next) {
   switch(req.params.type) {
   	case 'kelvin':
   	case 'k':
-  		res.send('69');
+  		res.send('69'); //todo real numbers and check fallthrough from previous case.
   }
 });
 
 router.get('/:devicekey/hi', function(req, res, next) {
-  
+  //todo heat index
 });
 
 router.get('/:devicekey/humidity', function(req, res, next) {
-  
+  //todo humidity
 });
 
 router.get('/:devicekey/:param/write', function(req, res, next) {
-  
+  //todo object parsing
 });
 
 module.exports = router;
